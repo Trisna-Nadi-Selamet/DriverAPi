df911f43693626594c3539db9dff05819be4564c
Dies Nadi <nadi.selamet@gmail.com>
2021-09-03 12:48:15 +0700

    update

--- server/controllers/AssignDriverController.js
+++ server/controllers/AssignDriverController.js
@@ -1,23 +1,45 @@
 
 const{driver}=require('../models')
+const {Op}=require('sequelize')
 class AssignDriverController{
     static async addassignDrivers(req,res){
         let name = req.params.name        
         try{
         let assign = await driver.findAll({
-        where:{
-            lat,
-            lon
+          where:{
+             
+            [Op.or]:[
+            {
+              lat:{
+                [Op.like]:'%' + search + '%',
+              },
+            },
+            {
+              lon: {
+                [Op.like]: '%' + search + '%',
+              },
+            },
 
-        },
-        
-        })       
-      res.status(200).json(assign)
-      }catch(error){
-        res.status(500).json(error)
-    
-      } 
+            ]
+          }
+        })   
+        if (driver.length > 0) {
+          res.status(200).json({
+            message: 'Data Semua Di Temukan',
+            data: driver,
+          });
+        } else {
+          res.status(200).json({
+            message: 'Data Tidak Di Temukan',
+            data: [],
+          });
+        }
+      } catch (error) {
+        res.status(500).json({
+          message: error,
+        });
+      }    
     }
-}
+  }
 
 module.exports = AssignDriverController
--- server/routes/AssignDriver.js
+++ server/routes/AssignDriver.js
@@ -2,7 +2,6 @@ const DriverRoute = require('express').Router();
 
 const AssignDriverController = require('../controllers/AssignDriverController')
 
-DriverRoute.get('/:lat/:lon',AssignDriverController.addassignDrivers)
-
+DriverRoute.get('/search/:lat',AssignDriverController.addassignDrivers)
 
 module.exports = DriverRoute
